0b262a48a273442d456a6e6172cbc2986b8cc6b9
Tatyana-Essentya <dlyarubicona@gmail.com>
2021-11-21 15:05:50 +0300

    123

--- src/router/index.js
+++ src/router/index.js
@@ -88,7 +88,23 @@ export const constantRoutes = [
         meta: { title: 'Form', icon: 'form' }
       }
     ]
+  },
+
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
   }
+
 ]
 
 /**
